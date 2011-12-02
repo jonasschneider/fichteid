c0a3f8691e56c745d116d3f4ab9e2d14fb978eec
Jonas Schneider <mail@jonasschneider.com>
2011-12-02 17:37:24 +0100

    add integration example

--- spec/requests/app_spec.rb
+++ spec/requests/app_spec.rb
@@ -26,6 +26,21 @@ describe "visiting /sso" do
     it "presents the requested realm" do
       get "/sso", params
       last_response.body.should =~ /#{realm}/
+      
+      module Fichteid
+        module LdapUser
+          def self.authenticated?(user, password)
+            true
+          end
+        end
+      end
+      
+      fill_in 'username', with: user
+      fill_in 'password', with: password
+      click_button 'Anmelden'
+      follow_redirect!
+      
+      last_response.should be_a_redirect_to_the_consumer(consumer_url, user)
     end
   end
 
@@ -64,6 +79,13 @@ describe "visiting /sso" do
         
         last_request.url.should == consumer_url
       end
+    end
+    
+    it "works in integration" do
+u = "/sso?openid.mode=checkid_setup&openid.identity=http%3a%2f%2fspecs.openid.net%2fauth%2f2.0%2fidentifier_select&openid.ns=http%3a%2f%2fspecs.openid.net%2fauth%2f2.0&openid.claimed_id=http%3a%2f%2fspecs.openid.net%2fauth%2f2.0%2fidentifier_select&openid.realm=http%3a%2f%2fwww.swedenintouch.se%2f&openid.return_to=http%3a%2f%2fwww.swedenintouch.se%2fTemplates%2fSignInOpenId.aspx%3fid%3d131%26token%3dGBjLQ3%252bJ4x0Lgqx2LZtHzh0pUZ9nLfshQBqlDA%252fW8P1odHRwOi8vc3BlY3Mub3BlbmlkLm5ldC9hdXRoLzIuMC9pZGVudGlmaWVyX3NlbGVjdA0KaHR0cDovL3NwZWNzLm9wZW5pZC5uZXQvYXV0aC8yLjAvaWRlbnRpZmllcl9zZWxlY3QNCmh0dHA6Ly9maWNodGVpZC5oZXJva3UuY29tL3Nzbw0KMi4wDQo%253d&openid.assoc_handle=%7bHMAC-SHA256%7d%7b4ed8f613%7d%7ba6SBpQ%3d%3d%7d&openid.ns.sreg=http%3a%2f%2fopenid.net%2fextensions%2fsreg%2f1.1&openid.sreg.required=&openid.sreg.optional=email%2cfullname%2cgender"
+      get u
+      click_button 'Fortfahren'
+      last_response.should be_a_redirect_to_the_consumer("http://www.swedenintouch.se/Templates/SignInOpenId.aspx?id=131&token=GBjLQ3%2bJ4x0Lgqx2LZtHzh0pUZ9nLfshQBqlDA%2fW8P1odHRwOi8vc3BlY3Mub3BlbmlkLm5ldC9hdXRoLzIuMC9pZGVudGlmaWVyX3NlbGVjdA0KaHR0cDovL3NwZWNzLm9wZW5pZC5uZXQvYXV0aC8yLjAvaWRlbnRpZmllcl9zZWxlY3QNCmh0dHA6Ly9maWNodGVpZC5oZXJva3UuY29tL3Nzbw0KMi4wDQo%3d", user)
     end
     
     
